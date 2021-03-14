41c8d8d84d1e288609832ce39a061ee2cb35bf49
Andrzej Baran <andrzej.baran@notowany.pl>
2021-03-14 22:42:32 +0100

    Próba js change + css środkowanie

--- main.js
+++ main.js
@@ -4,3 +4,11 @@ const age = 24;
 console.log("Na imię ma ", name, "i ma", age);
 console.log(`Tak na imię mam ${name} i mam ${age} lat.`);
 console.log("name jest deprecated???????");
+
+const klasa = document.querySelector('.artykul__tresc--js');
+
+console.log(klasa);
+
+klasa.innerHTML = "TO NOWA TRESC";
+
+console.log(klasa);
