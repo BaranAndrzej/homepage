7d3b9b3fcc83080c95dabb47101e6ba6e8ff0924
Andrzej Baran <andrzej.baran@notowany.pl>
2021-03-14 14:50:35 +0100

    Zabawy z css i mała kosmetyka

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-console.log('spadamy???');
-const name = 'Andrzej';
+console.log("spadamy???");
+const name = "Andrzej";
 const age = 24;
-console.log('Na imię ma ',name,'i ma',age);
+console.log("Na imię ma ", name, "i ma", age);
 console.log(`Tak na imię mam ${name} i mam ${age} lat.`);
-console.log('name jest deprecated???????')
+console.log("name jest deprecated???????");
